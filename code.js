e1f7c658e56e981bf4c895b37f912f7d6e27a85d
Veeled <vedhneeli@gmail.com>
2023-01-17 10:49:42 +1000

    loop function to create div containers

---
+++ code.js
@@ -0,0 +1,16 @@
+const container = document.querySelector('#container')
+
+// from https://stackoverflow.com/questions/57550082/creating-a-16x16-grid-using-javascript
+function makeDivs(rows, cols) {
+     //Creating a custom CSS property name '--grid-..'. Value is set by the variables: can be used to change the number of rows/columns in a grid
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+
+    //Loop which only runs until it reaches the multiplied value of makeDivs. Starting value is set by innerText
+    for (c = 0; c < (rows * cols); c++) {
+        let cell = document.createElement('div');
+        cell.innerText = (c + 1);
+        container.appendChild(cell).className = 'grid-item';
+    };
+};
+makeDivs(16, 16)
