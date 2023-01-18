3ea623bf40e8bebad1870918e4411fd3f19ef6b8
Veeled <vedhneeli@gmail.com>
2023-01-18 21:18:39 +1000

    adjusting how cell values are read and stored in rbgpercentvalue. Created multiplier to change the rgbColor (dictates what colour is displayed)

--- code.js
+++ code.js
@@ -4,19 +4,25 @@ function makeDivs(rows, cols) {
     container.innerHTML = "";
     container.style.setProperty('grid-template-rows', 'repeat(' + rows + ', 1fr)');
     container.style.setProperty('grid-template-columns', 'repeat(' + cols + ', 1fr)');
+    let multiplier = 2;
     for (let c = 0; c < (rows * cols); c++) {
         let cell = document.createElement('div');
         cell.innerText = '';
         container.appendChild(cell).className = 'grid-item';
         cell.addEventListener ('mouseover', (event) => {
             let target = event.target;
+            //
             let rbgPercentValue = parseInt(target.dataset.percent);
+                    multiplier += 0.1;
+            //
             if (isNaN(rbgPercentValue)) rbgPercentValue = 100;
             if (rbgPercentValue >= 10) {
-                rbgPercentValue -= 50;
+                rbgPercentValue -= 10;
                 target.dataset.percent = rbgPercentValue;
+                console.log (multiplier)
             }
-            let rgbColor = `rgb(${rbgPercentValue}%,${rbgPercentValue}%,${rbgPercentValue}%)`;
+            // rgb (red, green, blue)
+            let rgbColor = `rgb(${rbgPercentValue * multiplier}%,${rbgPercentValue}%,${rbgPercentValue}%)`;
             if (target !== container) {
                 target.style['background'] = rgbColor;
             }
