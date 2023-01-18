73f92f0540b30ce7392d2eed7242dfe911a80059
Veeled <vedhneeli@gmail.com>
2023-01-19 09:47:33 +1000

    commenting on code to understand how it works

--- code.js
+++ code.js
@@ -1,28 +1,35 @@
 const container = document.querySelector('#container');
 
 function makeDivs(rows, cols) {
+
+    //innerHTML to empty to container so child elements do not compound. The 'grid-template-..' properties were used to take user input to create the desired rows + cols. 1fr gives the size of the child element: dynamically adjusts to fit the container. 
     container.innerHTML = "";
     container.style.setProperty('grid-template-rows', 'repeat(' + rows + ', 1fr)');
     container.style.setProperty('grid-template-columns', 'repeat(' + cols + ', 1fr)');
     let multiplier = 2;
+
+    //Loop to create the grid/ child elements
     for (let c = 0; c < (rows * cols); c++) {
         let cell = document.createElement('div');
         cell.innerText = '';
         container.appendChild(cell).className = 'grid-item';
+        //Event to fire when mouse hovers over child elements
         cell.addEventListener ('mouseover', (event) => {
+            //storing the event/ cell in target so dont have to repeat event.target repeatedly. Target references the object in which the event was triggered.  
             let target = event.target;
-            //
+            //parseInt to convert string into integer. Custom attribute dataset to obtain the percentage value of cell. 
             let rbgPercentValue = parseInt(target.dataset.percent);
                     multiplier += 0.1;
-            //
+            //if rgbPercent.. is not a number/ is a string, return it as 100. 
             if (isNaN(rbgPercentValue)) rbgPercentValue = 100;
-            if (rbgPercentValue >= 10) {
-                rbgPercentValue -= 10;
-                target.dataset.percent = rbgPercentValue;
-                console.log (multiplier)
+           if (rbgPercentValue >= 10) {
+               rbgPercentValue -= 10;
+               //The previous cell percentage value is equal to the new set value
+               target.dataset.percent = rbgPercentValue;
             }
             // rgb (red, green, blue)
-            let rgbColor = `rgb(${rbgPercentValue * multiplier}%,${rbgPercentValue}%,${rbgPercentValue}%)`;
+            let rgbColor = `rgb(${rbgPercentValue * multiplier}%,${rbgPercentValue / multiplier}%,${rbgPercentValue}%)`;
+            // if target/ cell is not the container, style the background the value of rgbColor. 
             if (target !== container) {
                 target.style['background'] = rgbColor;
             }
