428473b079e56efe04f8e390f25f67f2115e44af
Veeled <vedhneeli@gmail.com>
2023-01-18 14:59:43 +1000

    dynamically adjusted child to fit parent

--- code.js
+++ code.js
@@ -1,52 +1,41 @@
-const container = document.querySelector('#container')
+const container = document.querySelector('#container');
 
-// from https://stackoverflow.com/questions/57550082/creating-a-16x16-grid-using-javascript
 function makeDivs(rows, cols) {
-     //Creating a custom CSS property name '--grid-..'. Value is set by the variables: can be used to change the number of rows/columns in a grid
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', cols);
-
-    //Loop which only runs until it reaches the multiplied value of makeDivs. Starting value is set by innerText
-    for (c = 0; c < (rows * cols); c++) {
+    container.innerHTML = "";
+    container.style.setProperty('grid-template-rows', 'repeat(' + rows + ', 1fr)');
+    container.style.setProperty('grid-template-columns', 'repeat(' + cols + ', 1fr)');
+    for (let c = 0; c < (rows * cols); c++) {
         let cell = document.createElement('div');
-        cell.innerText = ''//(c + 1)  
+        cell.innerText = '';
         container.appendChild(cell).className = 'grid-item';
         cell.addEventListener ('mouseover', (event) => {
-
-        let target = event.target
-        let rbgPercentValue = parseInt(target.dataset.percent);
-        if (isNaN(rbgPercentValue)) rbgPercentValue = 100;
-        if (rbgPercentValue >= 10) {
-        rbgPercentValue -= 50;
-        target.dataset.percent = rbgPercentValue;
-    }
-        let rgbColor = `rgb(${rbgPercentValue}%,${rbgPercentValue}%,${rbgPercentValue}%)`
-    
-        if (target !== container) {
-            target.style['background'] = rgbColor;
-        }
-    
-
-            //event.target.style.color = 'orange';
-            //setTimeout(() => {
-               // event.target.style.color = ""
-            //}, 500);
+            let target = event.target;
+            let rbgPercentValue = parseInt(target.dataset.percent);
+            if (isNaN(rbgPercentValue)) rbgPercentValue = 100;
+            if (rbgPercentValue >= 10) {
+                rbgPercentValue -= 50;
+                target.dataset.percent = rbgPercentValue;
+            }
+            let rgbColor = `rgb(${rbgPercentValue}%,${rbgPercentValue}%,${rbgPercentValue}%)`;
+            if (target !== container) {
+                target.style['background'] = rgbColor;
+            }
         }, false);
-    };
+    }
 };
-makeDivs(16, 16)
+makeDivs(16, 16);
 
-let btn = document.querySelector('.grid-value')
+let btn = document.querySelector('.grid-value');
 btn.addEventListener('click', () => {
-    const rows = prompt('Number of rows required?')
-    const cols = prompt('Number of columns required?')
+    const rows = prompt('Number of rows required?');
+    const cols = prompt('Number of columns required?');
     if (!isNaN(rows) && !isNaN(cols)) {
         if (rows <= 100 && cols <= 100) {
-            makeDivs (rows, cols)
+            makeDivs (rows, cols);
         } else {
-            alert ('Must Enter Values Less Than 100')
+            alert ('Must Enter Values Less Than 100');
         }
     } else {
-        alert('Must Enter Number Values Only')
+        alert('Must Enter Number Values Only');
     }
-    }) 
+});
